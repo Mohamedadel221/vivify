c20b5c34322f9db07ba71cf15262dd26aa0f274b
Mohamedadel221 <mohamed161119999@gmail.com>
2025-01-08 19:55:21 +0200

    solve-all-content

--- js/default-assets/mySwiperSlide.js
+++ js/default-assets/mySwiperSlide.js
@@ -4,9 +4,9 @@ var swiper = new Swiper(".swiper-containers", {
   navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
   breakpoints: {
     320: { slidesPerView: 1, spaceBetween: 5 },
-    640: { slidesPerView: 3, spaceBetween: 20 },
-    768: { slidesPerView: 4, spaceBetween: 20 },
-    1024: { slidesPerView: 4, spaceBetween: 30 },
+    640: { slidesPerView: 2, spaceBetween: 5 },
+    768: { slidesPerView: 3, spaceBetween: 5 },
+    1024: { slidesPerView: 4, spaceBetween: 10 },
   },
   autopLay:{
     delay:3000,
