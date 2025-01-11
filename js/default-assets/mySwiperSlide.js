12595103ca6b98b1a49d7a9a4f2831bfa98329fe
Mohamedadel221 <mohamed161119999@gmail.com>
2025-01-11 05:26:54 +0200

    finish

--- js/default-assets/mySwiperSlide.js
+++ js/default-assets/mySwiperSlide.js
@@ -1,12 +1,12 @@
 var swiper = new Swiper(".swiper-containers", {
   slidesPerView: 4,
-  spaceBetween: 0,
+  spaceBetween: 5,
   navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
   breakpoints: {
     320: { slidesPerView: 1, spaceBetween: 0 },
-    640: { slidesPerView: 2, spaceBetween: 0 },
-    768: { slidesPerView: 3, spaceBetween: 0 },
-    1024: { slidesPerView: 3, spaceBetween: 0 },
+    640: { slidesPerView: 2, spaceBetween: 10 },
+    768: { slidesPerView: 3, spaceBetween: 10 },
+    1024: { slidesPerView: 4, spaceBetween: 0 },
   },
   autoplay:{
     delay:1500,
